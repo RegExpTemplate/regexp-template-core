d98e26b894bca22d89c7538bec455dad63c2938e
André Jesus Brito <35274086+AndreJesusBrito@users.noreply.github.com>
2020-09-26 00:56:45 +0100

    perf: improved and added tests to function "getRegExpPattern"

--- tests/FromSrcWebTests.js
+++ tests/FromSrcWebTests.js
@@ -5,3 +5,24 @@ function assertEquals(a,b) {
   }
 }
 
+
+{
+  assertEquals("\\+", getRegExpPattern(/\+/));
+  assertEquals("\\\\+", getRegExpPattern(/\\+/));
+  assertEquals("a+b", getRegExpPattern(/a+b/));
+
+  assertEquals("\\?", getRegExpPattern(/\?/));
+
+  assertEquals("x(?=y)", getRegExpPattern(/x(?=y)/));
+  assertEquals("x(?!y)", getRegExpPattern(/x(?!y)/));
+  assertEquals("(?<=y)x", getRegExpPattern(/(?<=y)x/));
+  assertEquals("(?<!y)x", getRegExpPattern(/(?<!y)x/));
+
+  assertEquals("[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]", getRegExpPattern(/[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/));
+
+
+  assertEquals("\\/", getRegExpPattern(/\//));
+
+  assertEquals("\\\\", getRegExpPattern(/\\/));
+}
+
